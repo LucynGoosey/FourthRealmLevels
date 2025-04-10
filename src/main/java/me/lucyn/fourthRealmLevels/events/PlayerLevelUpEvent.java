ab696384e1e197704f65f7635335db261d6a55f0
LucynGoosey <didgeridev@gmail.com>
2025-04-10 07:30:47 -0400

    - Added PlayerLevelUpEvent

---
+++ src/main/java/me/lucyn/fourthRealmLevels/events/PlayerLevelUpEvent.java
@@ -0,0 +1,40 @@
+package me.lucyn.fourthRealmLevels.events;
+
+import me.lucyn.fourthrealm.RealmPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerLevelUpEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final RealmPlayer realmPlayer;
+    private final int level;
+
+
+    public PlayerLevelUpEvent(RealmPlayer realmPlayer, int level) {
+        this.realmPlayer = realmPlayer;
+        this.level = level;
+    }
+
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+
+    public RealmPlayer getRealmPlayer() {
+        return realmPlayer;
+    }
+
+    public Player getPlayer() {
+        return realmPlayer.getPlayer();
+    }
+
+
+    public int getLevel() {
+        return level;
+    }
+}
--- src/main/java/me/lucyn/fourthRealmLevels/listeners/ExpChangeListener.java
+++ src/main/java/me/lucyn/fourthRealmLevels/listeners/ExpChangeListener.java
@@ -1,7 +1,10 @@
 package me.lucyn.fourthRealmLevels.listeners;
 
 import me.lucyn.fourthRealmLevels.FourthRealmLevels;
+import me.lucyn.fourthRealmLevels.events.PlayerLevelUpEvent;
 import me.lucyn.fourthrealm.RealmPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class ExpChangeListener implements Listener {
@@ -14,7 +17,7 @@ public class ExpChangeListener implements Listener {
 
     }
 
-    @Override
+    @EventHandler
     public void onExpChange(org.bukkit.event.player.PlayerExpChangeEvent event) {
         RealmPlayer realmPlayer = fourthRealmLevels.fourthRealmCore.getPlayerData(event.getPlayer());
         realmPlayer.XP += event.getAmount();
@@ -24,6 +27,7 @@ public class ExpChangeListener implements Listener {
             realmPlayer.level++;
         }
         fourthRealmLevels.fourthRealmCore.setPlayerData(realmPlayer);
+        Bukkit.getPluginManager().callEvent(new PlayerLevelUpEvent(realmPlayer, realmPlayer.level));
     }
 
     public static int getXpToNextLevel(int level) {
